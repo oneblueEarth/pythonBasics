e8baaed80bf5553118ab1a1bf11d6b6b1b5ade4a
Chaman <navodayan99@gmail.com>
2023-12-01 17:23:44 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+class Books:
+    pages="800"
+    color="white"
+    author="H.C Verma"
+    subject="Physics"
+    pass
+fund_of_physics=Books()
+
+print(fund_of_physics.subject)
+print(fund_of_physics.author)
